a59680d1b0f2955039dc430cd252fbb2ed6fd929
JayPi4c <32594337+JayPi4c@users.noreply.github.com>
2018-01-10 20:15:09 +0100

    Mische die sortierte Final File
    Beim einlesen der Bilddaten in die FinalFile wurden alle Bilder sortiert eingelesen und gespeichert. Dies hat eventuell zu einem falschen Lernverhalten eines Neuronalen Netzes geführt. (Bei meinen persönlichen Tests hat das Netz immer gesagt, dass es sich bei den Daten um die letzte Gruppe handle, welche eingelesen wurde, in meinem Fall "7" für 1€)
    Die Shuffle Methode erstellt eine neue .csv Datei, welche alle Zeilen der sortierten Datei gemischt enthält.

--- Converter/Code/PngToCsv/src/com/JayPi4c/Main.java
+++ Converter/Code/PngToCsv/src/com/JayPi4c/Main.java
@@ -1,43 +1,44 @@
 package com.JayPi4c;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
 public class Main {
 
-	public static final String lineSeparator = System
-			.getProperty("line.separator");
+	public static final String lineSeparator = System.getProperty("line.separator");
 
 	public static void main(String args[]) throws IOException {
+
+		File finalFile = new File("location");
 
 		File allInOneFile = new File(getExecutionPath() + "/allData.csv");
 		if (!allInOneFile.exists())
 			allInOneFile.createNewFile();
 
-		BufferedWriter allWriter = new BufferedWriter(new FileWriter(
-				allInOneFile));
+		BufferedWriter allWriter = new BufferedWriter(new FileWriter(allInOneFile));
 
 		for (int i = 0; i <= 7; i++) {
 			File directory = new File(getExecutionPath() + "/" + i);
-			System.out.println("Directory " + directory.getAbsolutePath()
-					+ "; number of files inside: " + directory.list().length);
+			System.out.println("Directory " + directory.getAbsolutePath() + "; number of files inside: "
+					+ directory.list().length);
 			System.out.println();
 
-			File finalFile = new File(directory.getParentFile()
-					.getAbsolutePath() + "/" + i + ".csv");
+			finalFile = new File(directory.getParentFile().getAbsolutePath() + "/" + i + ".csv");
 
 			if (!finalFile.exists())
 				finalFile.createNewFile();
 
 			BufferedWriter BW = new BufferedWriter(new FileWriter(finalFile));
 			for (int j = 1; j <= directory.list().length; j++) {
-				String path = directory.getPath() + "/" + i + "IMG" + j
-						+ ".JPG";
+				String path = directory.getPath() + "/" + i + "IMG" + j + ".JPG";
 				System.out.println("expected Path: " + path);
 				File f = new File(path);
 
@@ -67,6 +68,8 @@ public class Main {
 		}
 		allWriter.close();
 
+		shuffle(finalFile);
+
 	}
 
 	public static String getExecutionPath() {
@@ -74,5 +77,40 @@ public class Main {
 		File file = new File(absolutePath);
 		absolutePath = file.getParentFile().toString();
 		return absolutePath;
+	}
+
+	public static void shuffle(File f) throws IOException {
+		int numberOfLines = getNumberOfLines(f);
+
+		File finalFile = new File(f.getParentFile().getAbsolutePath() + "/shuffledFile.csv");
+		finalFile.createNewFile();
+
+		ArrayList<Integer> pool = new ArrayList<Integer>();
+		for (int i = 0; i < numberOfLines; i++)
+			pool.add(i);
+
+		BufferedWriter BW = new BufferedWriter(new FileWriter(finalFile));
+		while (pool.size() > 0)
+			BW.write(getLineOfFile(pool.remove((int) (Math.random() * pool.size())), f));
+
+		BW.close();
+	}
+
+	public static String getLineOfFile(int line, File f) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		for (int i = 0; i < line; i++)
+			br.readLine();
+		String s = br.readLine();
+		br.close();
+		return s;
+	}
+
+	public static int getNumberOfLines(File f) throws IOException {
+		BufferedReader BR = new BufferedReader(new FileReader(f));
+		int numberOfLines = 0;
+		while (BR.readLine() != null)
+			numberOfLines++;
+		BR.close();
+		return numberOfLines;
 	}
 }
